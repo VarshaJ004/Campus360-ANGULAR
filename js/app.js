c6eec8f7fed46b954028b3662e2f33b2862b2bdd
VarshaJ004 <varshajohnson333@gmail.com>
2026-07-22 11:38:58 +0530

    worked on css and added more list

--- js/app.js
+++ js/app.js
@@ -1,23 +1,33 @@
-
-
 var app = angular.module('Campus360', []);
 
 app.controller('dashboardcontroller', function ($scope) {
+
     console.log("Dashboard controller is loaded");
-    $scope.collegename = "FISAT";
+
+    $scope.collegename = "Fisat";
     $scope.totalstudents = 1200;
     $scope.totalfaculty = 100;
+    $scope.fees = 25600;
+    $scope.today = new Date();
+    $scope.department = ["CSE", "ECE", "EEE", "MECH", "CIVIL"];
+    $scope.selectedDepartment = "CSE";
 
+
+
+    $scope.showdashboard = true;
+    $scope.showAddStudentForm = false;
 
     $scope.students = [
         { name: "Varsha" },
         { name: "John" },
-        { name: "Alice"},
+        { name: "Alice" },
         { name: "Bob" }
     ];
 
     $scope.addStudent = function () {
         $scope.totalstudents++;
-    }
+    };
 
 });
+
+
